5890d0d3d17d2c5f92b0269a77999d7894240c67
DongND <dongnd@epayjsc.com>
2024-08-07 11:33:42 +0700

    update dockerfile

--- EPAY.AIRWAY.KIOSK.API/Resources/DTOs/Account/Validation/CreateValidator.cs
+++ EPAY.AIRWAY.KIOSK.API/Resources/DTOs/Account/Validation/CreateValidator.cs
@@ -8,7 +8,7 @@ public class CreateValidator : AbstractValidator<CreateRequest>
 {
     public CreateValidator()
     {
-        RuleFor(x => x.UserName).NotEmpty().NotNull().Must(x => x.Length <= 150 && Base64.IsValid(x));
+        RuleFor(x => x.UserName).NotEmpty().NotNull().Must(x => x.Length <= 150);
         RuleFor(x => x.Password).NotEmpty().NotNull().Must(x => x.Length >= 6 && Base64.IsValid(x));
         RuleFor(x => x.Email).EmailAddress().When(x => !string.IsNullOrEmpty(x.Email));
         RuleFor(x => x.SystemRoles).Must(x => x.TrueForAll(MyPolicy.IsValid));

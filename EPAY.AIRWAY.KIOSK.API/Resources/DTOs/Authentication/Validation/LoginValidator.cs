7feb5aa2d0d1fdc76acb8ca18e8fc8bccb01480f
DongND <dongnd@epayjsc.com>
2024-08-23 13:07:43 +0700

    bo sung validate

---
+++ EPAY.AIRWAY.KIOSK.API/Resources/DTOs/Authentication/Validation/LoginValidator.cs
@@ -0,0 +1,27 @@
+using System.Buffers.Text;
+using EPAY.AIRWAY.KIOSK.API.Resources.DTOs.Authentication.Request;
+using FluentValidation;
+
+namespace EPAY.AIRWAY.KIOSK.API.Resources.DTOs.Authentication.Validation;
+
+public class LoginValidator : AbstractValidator<LoginRequest>
+{
+    public LoginValidator()
+    {
+        RuleFor(x => x.UserName)
+            .NotEmpty()
+            .NotNull()
+            .Must(x => x.Length <= 150);
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .NotNull()
+            .Must(x => x.Length >= 6 && Base64.IsValid(x));
+
+        RuleFor(x => x.Type)
+            .NotEmpty()
+            .NotNull()
+            .Must(x => Enum.IsDefined(x!.Value))
+            .When(x => x.Type != null);
+    }
+}

5ee2c12af43d10ac692fd1314f540b580b75f487
DongND <dongnd@epayjsc.com>
2024-10-30 16:02:49 +0700

    cap nhat gitignore

--- EPAY.AIRWAY.KIOSK.API/Resources/Results/BaseResult.cs
+++ EPAY.AIRWAY.KIOSK.API/Resources/Results/BaseResult.cs
@@ -3,6 +3,7 @@
 public class BaseResult<T>
 {
     #region Properties
+
     [JsonPropertyName("data")]
     public T? Data { get; init; }
 
@@ -11,31 +12,46 @@ public class BaseResult<T>
     public CodeMessage CodeMessage { get; init; }
 
     [JsonPropertyName("statusCode")]
-    public string? StatusCode { get => CodeMessage.GetElementNameCodeMessage(); }
+    public string? StatusCode
+    {
+        get => CodeMessage.GetElementNameCodeMessage();
+    }
 
     [JsonPropertyName("message")]
-    public string? Message { get => _message; init => _message = GetMessage(value); }
+    public string? Message
+    {
+        get => _message;
+        init => _message = GetMessage(value);
+    }
+
     private string? _message;
+
     #endregion
 
     #region Constructor
-    public BaseResult() { }
 
-    public BaseResult(CodeMessage codeMessage, string? message = "") 
+    public BaseResult()
+    {
+    }
+
+    public BaseResult(CodeMessage codeMessage, string? message = "")
     {
         Data = default(T);
         CodeMessage = codeMessage;
         Message = message ?? string.Empty;
     }
+
     #endregion
 
     #region Method
+
     private string GetMessage(string? input)
     {
-        if(string.IsNullOrEmpty(input))
+        if (string.IsNullOrEmpty(input))
             return ResponseMessage.Values.TryGetValue(StatusCode ?? string.Empty, out var value) ? value : string.Empty;
 
         return input.RemoveSpaceCharacter();
     }
+
     #endregion
 }
